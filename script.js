a17017b429a6d400228724a1c4630ba510f3606c
OKratul <102370110+OKratul@users.noreply.github.com>
2022-05-29 14:27:37 +0600

    fibonacci numb

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// let arr = [25,36,65,92];
+// let sum = 0 ;
+
+// for( let singleItem of arr){
+//     sum = singleItem + sum ;
+// }
+// document.write(sum);
+
+let a = 5 ;
+let b = 6 ;
+
+for(let c = 0; c <= 7; c++){
+    let fibo = a + b ;
+    a = b; 
+    b = fibo ;
+    document.write(fibo);
+}
+
+// for( let i = 0;i < arr.length; i++ ){
+//   let result = arr[i];
+//   sum = sum + result ;
+// }
+// document.write(sum);
